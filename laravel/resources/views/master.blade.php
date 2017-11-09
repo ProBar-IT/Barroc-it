f21be268f3fd90f882f9be482c58614516a52d6e
Titson <tomaszt@hotmail.nl>
2017-11-09 13:07:22 +0100

    Revert "Deleted button by title"
    This reverts commit 58701a85db9800643e27b6b80da808eba69cbccd.

--- laravel/resources/views/master.blade.php
+++ laravel/resources/views/master.blade.php
@@ -21,8 +21,13 @@
         <nav class="navbar navbar-default navbar-inverse">
             <div class="container">
                 <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1">
+                        <p class="sr-only">Toggle Navigation</p>
+                    </button>
                     <a class="navbar-brand" href="#">Barroc-it</a>
                 </div>
+
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
                     <ul class="nav navbar-nav navbar-right">
